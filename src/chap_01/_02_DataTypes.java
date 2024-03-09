b18fdbe93461080a08b3882e242c6aa780eb0263
evan <guarneri1682@gmail.com>
2024-03-09 03:33:41 -0800

    first_java

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("hihi");
+        System.out.println("hello hi");
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println(false);
+
+    }
+}
