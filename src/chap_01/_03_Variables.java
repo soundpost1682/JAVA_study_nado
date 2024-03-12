f972eaa4923312394ebf48d6577e45b47aa22cfa
evan <guarneri1682@gmail.com>
2024-03-12 02:08:19 -0700

    var01

---
+++ src/chap_01/_03_Variables.java
@@ -0,0 +1,21 @@
+package chap_01;
+
+public class _03_Variables {
+    public static void main(String[] args) {
+        String name = "eva";
+        int hour = 15;
+        double score = 90.5;
+        char grade = 'A';
+        name = "강백호";
+
+        System.out.println(name + " delivery will arrive at " + hour);
+        System.out.println(name + " 평균점수는 " + score + " 점입니다.");
+        System.out.println("학점은 " + grade + " 이다.");
+
+        boolean pass = false;
+        System.out.println("합격했냐? " + pass);
+
+
+    }
+
+}
