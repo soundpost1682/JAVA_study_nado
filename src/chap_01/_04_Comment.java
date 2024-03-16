07cb4702f17d13f4c8f3656d21e1ce8ef2497bac
evan <guarneri1682@gmail.com>
2024-03-15 17:52:15 -0700

    240315_comment

---
+++ src/chap_01/_04_Comment.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+        System.out.println("(10 mins before) Let's prepare the Wedding.");
+        //System.out.println("(5 mins before) Let's prepare the Wedding.");
+        System.out.println("Now is the time to start.");
+
+        int size = 120;
+        size = size + 10; // children grow so fast
+        System.out.println("Please show me the " + size + " mm ok?");
+
+
+    }
+}
